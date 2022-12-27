85e75441c550e00456866306632b4268f45b772f
Geshman Shakeel <113760897+Geshman1323@users.noreply.github.com>
2022-12-27 16:56:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const clock = () => {
-    let hour = document.querySelector("#h");
-    let minutes = document.querySelector("#m");
-    let second = document.querySelector("#s");
-    let me = document.querySelector("#M");
-    let dd = new Date();
-    let h = dd.getHours();
-    let m = dd.getMinutes();
-    let s = dd.getSeconds();
-  
-    h > 12 ? (me.innerHTML = "PM") : (me.innerHTML = "AM");
-  
-    if (h > 12) {
-      h = h - 12;
-    }
-    if (h == 0) {
-      h = h + 12;
-    }
-  
-    hour.innerHTML = h;
-    minutes.innerHTML = m;
-    second.innerHTML = s;
-  };
-  
-  setInterval(clock, 500);
-  
-  // geshman1343
-  
+  let hourItem = document.querySelector("#h");
+  let minuteItem = document.querySelector("#m");
+  let secondItem = document.querySelector("#s");
+  let meridianItem = document.querySelector("#M");
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  hours > 12
+    ? (meridianItem.innerHTML = "PM")
+    : (meridianItem.innerHTML = "AM");
+  hours > 12 ? (hours = hours - 12) : (hours = hours);
+  hours == 0 ? (hours = hours + 12) : (hours = hours);
+
+  hourItem.innerHTML = hours;
+  minuteItem.innerHTML = minutes;
+  secondItem.innerHTML = seconds;
+};
+
+setInterval(clock, 500);
